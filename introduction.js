c3ac349a2d9c3fda150e80424342d932fdba19eb
iamregha <iamregha@gmail.com>
2024-05-26 23:16:21 +0100

    string concat

--- introduction.js
+++ introduction.js
@@ -1,4 +1,4 @@
 console.log("Welcome to 30days of JavaScript from an external js file!")
 
-let firstName
-console.log(firstName)
+let firstName = 'Abraham' //the value is a string
+console.log('My First name is ' +firstName)
