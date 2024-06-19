50d1e87fb22048e672d4adb3eebc15abf51a591f
Regha <iamregha@gmail.com>
2024-06-19 17:28:55 +0100

    using back-ticks to create template strings

--- day2.js
+++ day2.js
@@ -1,1 +1,6 @@
-console.log('The sum of 3 and 2 is five')
+console.log(`The sum of 3 and 2 is 5`) // Statically writting the data
+
+let a = 3
+let b = 2
+
+console.log(`The sum of ${a} and ${b} is ${a+b}`) // Injecting the data dynamically
