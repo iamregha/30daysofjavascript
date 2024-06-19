958e04d948078ba046e38cff57bc14107605a2ee
Regha <iamregha@gmail.com>
2024-06-19 19:19:03 +0100

    using length--one of string methods

--- day2.js
+++ day2.js
@@ -2,7 +2,10 @@ console.log(`The sum of 3 and 2 is 5`) // Statically writting the data
 
 let a = 3
 let b = 2
+let js = 'JavaScript'
 
 console.log(`The sum of ${a} and ${b} is ${a+b}`) // Injecting the data dynamically
 
 console.log(`${a} is greater than ${b}: ${a > b}`)
+
+console.log(js.length)
