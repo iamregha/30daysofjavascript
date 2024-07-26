8fdf910b782d0ee3c6fd03fe4e84fff127862d7d
Regha <iamregha@gmail.com>
2024-07-27 00:40:48 +0100

    conditionals

--- exercises/day3_2.js
+++ exercises/day3_2.js
@@ -1,6 +1,14 @@
-let b = prompt("Enter the base of the triangle: ")
+/*let b = prompt("Enter the base of the triangle: ")
 let h = prompt("Enter the height", "The height of the triangle")
 
 let theArea = (0.5*b*h)
 
-console.log(theArea)
+console.log(theArea)*/
+
+let Age = prompt("Enter Birth year")
+let year = new Date()
+let myAge = year.getFullYear() - Age
+
+if (year.getFullYear() - Age >= 18){
+    alert(`You are ${myAge} now you can drive`)
+}
