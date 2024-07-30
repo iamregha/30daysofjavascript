a258854bce8272116e0daa9dcfca40cc47f7110c
Regha <iamregha@gmail.com>
2024-07-30 13:03:56 +0100

    using a combination of if and switch statements

--- exercises/day3_2.js
+++ exercises/day3_2.js
@@ -9,10 +9,27 @@ let Age = prompt("Enter Birth year")
 let year = new Date()
 let myAge = year.getFullYear() - Age
 
+/*Using ifelse if
+/*
 if (Age != "" && myAge >= 18){
     alert(`You are ${myAge} now you can drive`)
 } else if(Age != "" && myAge < 18){
     alert(`You are ${myAge} and less than 18, you can't drive alone`)
 } else {
     alert("The field can't be empty... please enter your birth year")
-} 
+} */
+
+/*Using switch*/
+if (Age != "") {
+    switch (true) {
+        case myAge >= 18:
+            alert(`You're ${myAge} now you can drive`)
+            break
+        case myAge < 18:
+            alert(`You're ${myAge}, you're less than 18. Sorry you can't drive`)
+            break
+    }
+}
+    else {
+        alert("The field can't be empty... please enter your birth year")
+}
